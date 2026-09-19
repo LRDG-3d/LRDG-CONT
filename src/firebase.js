4b6dc6e7bc15ab64fd400fff79e95636f64149a0
LRDG-3d <LRDG-3d>
2026-09-18 23:38:10 -0600

    fix firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyClpc5dBi_QQclvlCA9KjRYy0lSJgTjDFc",
   authDomain: "lrdg-cont.firebaseapp.com",
@@ -14,5 +12,7 @@ const firebaseConfig = {
   appId: "1:37780270749:web:fe96932d269c0abbb7ae74"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
